87233f85030b881e43f5da69a40449914ff5319e
temanbi4 <temanbi4@gmail.com>
2023-12-30 05:59:55 +0300

    version for docker v1

--- KYC/KYC/settings.py
+++ KYC/KYC/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-#        'HOST': os.getenv('DB_HOST')
+        'HOST': os.getenv('DB_HOST')
     }
 }
 
